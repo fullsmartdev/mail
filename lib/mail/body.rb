4d8e107eac3995bccb208254c8aaf62b43f3b364
HideAKi <hideaki.chain@gmail.com>
2011-02-24 18:09:11 -0800

    Fixed a NilClass (NoMethodError) when the In-Reply-To or References fields are present but empty and you call to_s on an instance of Mail.

--- lib/mail/body.rb
+++ lib/mail/body.rb
@@ -162,12 +162,7 @@ module Mail
         else
             # Decode then encode to normalize and allow transforming 
             # from base64 to Q-P and vice versa
-            decoded = dec.decode(raw_source)
-            if defined?(Encoding) && charset && charset != "US-ASCII"
-              decoded.encode!(charset)
-              decoded.force_encoding('BINARY') unless Encoding.find(charset).ascii_compatible?
-            end
-            enc.encode(decoded)
+            enc.encode(dec.decode(raw_source))
         end
       end
     end
