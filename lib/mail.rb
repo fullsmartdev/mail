528fd70019923b99d3f359e66bd8894040eaa118
HideAKi <hideaki.chain@gmail.com>
2012-06-05 15:14:43 -0500

    Do not require Net::IMAP or Net::POP if they're already loaded

--- lib/mail.rb
+++ lib/mail.rb
@@ -46,16 +46,6 @@ module Mail # :doc:
   require 'mail/utilities'
   require 'mail/configuration'
 
-  @@autoloads = {}
-  def self.register_autoload(name, path)
-    @@autoloads[name] = path
-    autoload(name, path)
-  end
-
-  def self.eager_autoload!
-    @@autoloads.each { |_,path| require(path) }
-  end
-
   # Autoload mail send and receive classes.
   require 'mail/network'
 
